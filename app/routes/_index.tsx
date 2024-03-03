74da2135166c195f28a49f545e1dff7648b12c2d
Bohao Wang <bj050323@gmail.com>
2024-03-03 23:13:08 +0800

    feat: trend list

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,40 +1,40 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { desc } from 'drizzle-orm';
+import { RepositorySchema, db } from "~/db/config.server";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Trender Remix" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
+export const loader = async () => {
+  const result = await db.select().from(RepositorySchema)
+    .orderBy(desc(RepositorySchema.created), desc(RepositorySchema.star)).limit(50);
+  return json(result);
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }} className="p-4">
+      <h1 className="text-xl py-4">Github Trend</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.map(item => (
+          <li key={item.id} className="flex">
+            <span className="w-20">{item.star}</span>
+            <a
+              target="_blank"
+              href={item.url!}
+              rel="noreferrer"
+              className="text-blue-400"
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
