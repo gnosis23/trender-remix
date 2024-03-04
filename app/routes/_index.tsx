b4c7f1fcba272135efef051d5d14cd5a3489d6ca
Bohao Wang <bj050323@gmail.com>
2024-03-04 18:43:18 +0800

    fix: repository order

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import { json, type MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { desc } from 'drizzle-orm';
+import { desc, gt } from "drizzle-orm";
 import { RepositorySchema, db } from "~/db/config.server";
 
 export const meta: MetaFunction = () => {
@@ -11,18 +11,27 @@ export const meta: MetaFunction = () => {
 };
 
 export const loader = async () => {
-  const result = await db.select().from(RepositorySchema)
-    .orderBy(desc(RepositorySchema.created), desc(RepositorySchema.star)).limit(50);
+  const date = new Date();
+  date.setDate(date.getDate() - 3);
+  const result = await db
+    .select()
+    .from(RepositorySchema)
+    .where(gt(RepositorySchema.created, date))
+    .orderBy(desc(RepositorySchema.star))
+    .limit(50);
   return json(result);
 };
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }} className="p-4">
+    <div
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+      className="p-4"
+    >
       <h1 className="text-xl py-4">Github Trend</h1>
       <ul>
-        {data.map(item => (
+        {data.map((item) => (
           <li key={item.id} className="flex">
             <span className="w-20">{item.star}</span>
             <a
