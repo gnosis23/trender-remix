71d935eb2d2730354a8d55f3d51d8a03422bec7b
Bohao Wang <bj050323@gmail.com>
2024-03-05 22:22:33 +0800

    feat: support like!

--- app/db/config.server.ts
+++ app/db/config.server.ts
@@ -1,12 +1,13 @@
 import {
+  int,
   mysqlTable,
   serial,
-  varchar,
-  int,
   timestamp,
+  varchar,
 } from "drizzle-orm/mysql-core";
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
+import { desc, eq, gt } from "drizzle-orm";
 
 // Create the connection
 const poolConnection = mysql.createPool(process.env.DATABASE_URL as string);
@@ -30,3 +31,32 @@ export const UserSchema = mysqlTable("user", {
   username: varchar("username", { length: 64 }),
   password: varchar("password", { length: 128 }),
 });
+
+export const LikeSchema = mysqlTable("like", {
+  id: serial("id").primaryKey(),
+  userId: int("user_id"),
+  repoId: int("repo_id"),
+  like: int("like"),
+  created: timestamp("created"),
+});
+
+export const fetchRepositories = async (date: Date, userId: number) => {
+  const like = db
+    .select()
+    .from(LikeSchema)
+    .where(eq(LikeSchema.userId, userId))
+    .as("like");
+  return db
+    .select({
+      id: RepositorySchema.id,
+      name: RepositorySchema.name,
+      star: RepositorySchema.star,
+      url: RepositorySchema.url,
+      like: LikeSchema.like,
+    })
+    .from(RepositorySchema)
+    .leftJoin(like, eq(RepositorySchema.id, like.repoId))
+    .where(gt(RepositorySchema.created, date))
+    .orderBy(desc(RepositorySchema.star))
+    .limit(50);
+};
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,19 @@
-import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
-import { desc, gt } from "drizzle-orm";
-import { RepositorySchema, db } from "~/db/config.server";
+import {
+  ActionFunctionArgs,
+  json,
+  LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import {
+  Form,
+  Link,
+  useFetcher,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
+import { fetchRepositories } from "~/db/config.server";
 import { requireUser } from "~/session.session";
+import { addLike, fetchLike, updateLike } from "~/db/like.server";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,16 +23,10 @@ export const meta: MetaFunction = () => {
 };
 
 export const loader = async (c: LoaderFunctionArgs) => {
+  const user = await requireUser(c.request);
   const date = new Date();
   date.setDate(date.getDate() - 3);
-  const repositories = await db
-    .select()
-    .from(RepositorySchema)
-    .where(gt(RepositorySchema.created, date))
-    .orderBy(desc(RepositorySchema.star))
-    .limit(50);
-
-  const user = await requireUser(c.request);
+  const repositories = await fetchRepositories(date, user.userId);
 
   return json({
     repositories,
@@ -29,8 +34,44 @@ export const loader = async (c: LoaderFunctionArgs) => {
   });
 };
 
+export const action = async (c: ActionFunctionArgs) => {
+  const user = await requireUser(c.request);
+  const data = await c.request.formData();
+  const form = Object.fromEntries(data);
+
+  const { id, favor } = form;
+  const repoId = +id;
+  const like = favor === "1" ? 1 : 0;
+  if (Number.isNaN(repoId)) {
+    return null;
+  }
+
+  const record = await fetchLike(user.userId, repoId);
+  if (record) {
+    await updateLike(record.id, like);
+  } else {
+    await addLike(user.userId, repoId, like);
+  }
+
+  return null;
+};
+
 export default function Index() {
   const data = useLoaderData<typeof loader>();
+  const fetcher = useFetcher();
+
+  const navigation = useNavigation();
+
+  const onFavor = (id: number, favor: string) => {
+    if (navigation.state === "submitting") return;
+    fetcher.submit(
+      { id, favor },
+      {
+        method: "post",
+      },
+    );
+  };
+
   return (
     <div
       style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
@@ -57,12 +98,22 @@ export default function Index() {
 
       <ul className="mt-4">
         <li className="flex leading-8">
-          <span className="w-20 shrink-0">Stars</span>
+          <span className="w-12 shrink-0">Stars</span>
+          <div className="w-12">Like</div>
           <div>Name</div>
         </li>
-        {data.repositories.map((item) => (
+        {data.repositories.map((item, index) => (
           <li key={item.id} className="flex leading-8">
-            <span className="w-20 shrink-0">{item.star}</span>
+            <span className="w-12 shrink-0">{item.star}</span>
+            <div
+              role="button"
+              tabIndex={index}
+              className="w-6 mr-6 cursor-pointer text-yellow-500"
+              onClick={() => onFavor(item.id, item.like === 1 ? "0" : "1")}
+              onKeyDown={() => onFavor(item.id, item.like === 1 ? "0" : "1")}
+            >
+              {item.like === 1 ? "★" : "☆"}
+            </div>
             <a
               target="_blank"
               href={item.url!}
