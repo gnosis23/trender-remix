dab2c72a9042978d91c4a854b8079aa855ff9632
Bohao Wang <bj050323@gmail.com>
2024-03-07 10:36:05 +0800

    feat: pending link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,24 @@
 import { Form, NavLink } from "@remix-run/react";
 
+const linkClass = ({
+  isActive,
+  isPending,
+}: {
+  isActive: boolean;
+  isPending: boolean;
+}) =>
+  `p-1 rounded-lg hover:bg-gray-200 ${
+    isActive ? "text-blue-400" : isPending ? "text-blue-200" : "text-gray-400"
+  }`;
+
 export default function Header() {
   return (
     <div className="flex justify-between items-center">
       <div className="flex space-x-4">
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            `p-1 rounded-lg hover:bg-gray-200 ${
-              isActive ? "text-blue-400" : "text-gray-600"
-            }`
-          }
-        >
+        <NavLink to="/" className={linkClass}>
           Home
         </NavLink>
-        <NavLink
-          to="/like"
-          className={({ isActive }) =>
-            `p-1 rounded-lg hover:bg-gray-200 ${
-              isActive ? "text-blue-400" : "text-gray-600"
-            }`
-          }
-        >
+        <NavLink to="/like" className={linkClass}>
           Like
         </NavLink>
       </div>
